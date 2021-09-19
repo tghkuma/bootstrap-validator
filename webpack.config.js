811e4469d9e919e55e67ed3f5bde2dfbcc5bb9f5
tghkuma <info@team-grasshopper.info>
2021-09-20 02:06:23 +0900

    feat(webpack): webpack5対応
    webpack5対応
    umd,esm対応
    umd版サンプル実装
    html5バリデーション(参考)でBootstrapエラースタイル適用
    READMEを本パッケージ版に描き直し

---
+++ webpack.config.js
@@ -0,0 +1,63 @@
+const path = require('path');
+const entry = {
+  'bootstrap-validator': [
+    './src/messages/ja.js',
+    './src/bootstrap-validator.js'
+  ],
+};
+const umdConfig = {
+  mode: 'development',
+  entry: entry,
+  output: {
+    path: path.resolve(__dirname, 'js'),
+    filename: `[name].js`,
+    libraryTarget: 'umd',
+  },
+  devtool: false,
+};
+const umdMinConfig = {
+  mode: 'production',
+  entry: entry,
+  output: {
+    path: path.resolve(__dirname, 'js'),
+    filename: `[name].min.js`,
+    libraryTarget: 'umd',
+  },
+  // devtool: 'source-map',
+};
+const esmConfig = {
+  mode: 'development',
+  entry: entry,
+  output: {
+    path: __dirname + '/js',
+    filename: `[name].esm.js`,
+    library: {
+      type: "module",
+    },
+  },
+  experiments: {
+    outputModule: true,
+  },
+  devtool: false,
+};
+const esmMinConfig = {
+  mode: 'production',
+  entry: entry,
+  output: {
+    path: __dirname + '/js',
+    filename: `[name].esm.min.js`,
+    library: {
+      type: "module",
+    },
+  },
+  experiments: {
+    outputModule: true,
+  },
+  // devtool: 'source-map',
+};
+module.exports = [
+  umdConfig,
+  umdMinConfig,
+  esmConfig,
+  esmMinConfig
+];
